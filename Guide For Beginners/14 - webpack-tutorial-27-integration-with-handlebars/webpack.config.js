40f4183a83fa32740343e323a053d68a37bfef09
Deepk kumar <deepakanjani21@gmail.com>
2025-08-16 22:36:11 +0530

    14 - webpack-tutorial-27-integration-with-handlebars

---
+++ Guide For Beginners/14 - webpack-tutorial-27-integration-with-handlebars/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: 'asset/source',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ['handlebars-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        path.join(process.cwd(), 'build/**/*'),
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Hello world', // Title of the HTML document declared in the <title> tag using Handlebars
+      template: 'src/index.hbs',
+      description: 'Some description', // Description of the HTML document declared in the <title> tag using Handlebars
+    }),
+  ],
+};
