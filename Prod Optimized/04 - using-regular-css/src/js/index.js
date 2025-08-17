32eb6eabed4f6bda3fecde6813986b074ccf50be
Deepk kumar <deepakanjani21@gmail.com>
2025-08-18 00:08:35 +0530

    css loader

---
+++ Prod Optimized/04 - using-regular-css/src/js/index.js
@@ -0,0 +1,22 @@
+import {
+  onLoadEventHandler,
+  newTodoEventHandler,
+  removeTodoEventHandler,
+  toggleTodoEventListener,
+} from './event-handlers';
+import '../styles/index.css'; // Importing CSS file in js to apply styles and removed from index.html
+
+window.addEventListener('load', onLoadEventHandler);
+document.addEventListener('change', function (event) {
+  if (event.target.classList.contains('new-todo')) {
+    newTodoEventHandler(event);
+  }
+});
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('delete')) {
+    removeTodoEventHandler(event);
+  }
+  if (event.target.classList.contains('real-checkbox')) {
+    toggleTodoEventListener(event);
+  }
+});
